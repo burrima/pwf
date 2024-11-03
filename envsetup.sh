6e873dbb0b328def04f42611230dbc579fa3f8a1
Martin Burri <info@burrima.ch>
2024-11-03 12:01:02 +0100

    update envsetup.sh to make it work with new python scripts

--- envsetup.sh
+++ envsetup.sh
@@ -24,25 +24,20 @@ for tool in fzf exiv2 convert cksum md5sum ffmpeg; do
     [[ $(type -P $tool) ]] || >&2 echo "ERROR: $tool not installed!"
 done
 
-# Setup special prompt
-export PS1="(pwf) $PS1"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+(cd $SCRIPT_DIR && python3 -m venv venv --prompt pwf)
+. "$SCRIPT_DIR"/venv/bin/activate
 
 # Add bin folder to PATH
-export PATH="$(pwd)/bin/:$PATH"
+export PATH="$SCRIPT_DIR/bin/:$PATH"
+export PYTHONPATH="$SCRIPT_DIR/bin/:$PYTHONPATH"
 
 # define global variables for pwf scripts
 export PWF_ALLOWED_CHARACTERS='[:alnum:]äöüÄÖÜé~._-'
-
 export PWF_HOME="$HOME/pictures/own_photos/"
 [[ "$PWF_HOME" =~ ^[//$PWF_ALLOWED_CHARACTERS]*$ ]] || \
     >&2 echo "ERROR: PWF_HOME path contains illegal characters!"
 # PWF_HOME can be considered save from now on...
-
-export PWF_RAW_EXTENSIONS="NEF NRW CR2"
-export PWF_JPG_EXTENSIONS="jpg JPG jpeg JPEG"
-
-export PWF_RAW_REGEX='^.*\.(NEF|NRW|CR2)'  # can be used e.g. with [[ =~ ]]
-export PWF_JPG_REGEX='^.*\.(jpg|JPG|jpeg|JPEG)'  # can be used e.g. with [[ =~ ]]
 
 # Set up fzf environment:
 export FZF_CTRL_T_COMMAND="find -L $PWF_HOME"
@@ -61,40 +56,18 @@ _fzf_compgen_dir() {
     $FZF_ALT_C_COMMAND
 }
 
-_pwf_err_exit() {
-    >&2 echo "ERROR: $1"
-    exit 1
-}
-export -f _pwf_err_exit
-
-_pwf_is_event_dir() {
-    [[ $(dirname "$1") =~ [0-9]{4} ]] && return 0 || return 1
-}
-export -f _pwf_is_event_dir
-
-_pwf_print_fzf_info() {
-    cat <<'EOF'
-    Any path can be specified either by by normal means of bash (e.g. with tab
-    completion) or by using the FZF tool. Type **<tab> to bring the FZF tool to 
-    front where you can select the desired path much faster (see man fzf for 
-    more details). Paths are automatically bound to the pwf folder structure,
-    so this script can be used from anywhere whith consistent behavior.
-EOF
-}
-export -f _pwf_print_fzf_info
-
 # Enable fuzzy completion for pwf commands
 alias ll >/dev/null 2>&1 || alias ll="ls -alF"
 _fzf_setup_completion dir ll
-_fzf_setup_completion dir pwf-check
-_fzf_setup_completion path pwf-extract-previews
-_fzf_setup_completion dir pwf-move-new-to-original
-_fzf_setup_completion path pwf-protect
-_fzf_setup_completion dir pwf-prepare-lab
-_fzf_setup_completion dir pwf-statistics
-_fzf_setup_completion path pwf-rename-by-date
-_fzf_setup_completion dir pwf-cleanup
-_fzf_setup_completion path pwf-downsize-image
-_fzf_setup_completion path pwf-downsize-video
-_fzf_setup_completion path pwf-link
+_fzf_setup_completion dir pwf_check.py
+_fzf_setup_completion path pwf_extract_previews.py
+_fzf_setup_completion dir pwf_import.py
+_fzf_setup_completion path pwf_protect.py
+_fzf_setup_completion dir pwf_prepare_lab.py
+_fzf_setup_completion dir pwf_statistics.py
+_fzf_setup_completion path pwf_rename_by_date.py
+_fzf_setup_completion dir pwf_cleanup.py
+_fzf_setup_completion path pwf_downsize_image.py
+_fzf_setup_completion path pwf_downsize_video.py
+_fzf_setup_completion path pwf_link.py
 
